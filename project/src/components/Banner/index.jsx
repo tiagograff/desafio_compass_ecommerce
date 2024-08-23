940236d896c40169277b4c0ac675102844676123
tiago <tiagopinzgraffunder@gmail.com>
2024-08-23 14:48:26 -0300

    fix: tirando estilização do id do menu. feat: adicionando banner

---
+++ project/src/components/Banner/index.jsx
@@ -0,0 +1,14 @@
+import "../../styles/banner.css";
+
+export default function Banner() {
+  return (
+    <section className="banner-content">
+      <div className="banner">
+        <h2 className="banner-title">Shop</h2>
+        <p className="paragraph-banner">
+          <span className="bold-text">Home {">"} </span>Shop
+        </p>
+      </div>
+    </section>
+  );
+}
--- project/src/components/Header/NavBar.jsx
+++ project/src/components/Header/NavBar.jsx
@@ -4,9 +4,7 @@ export default function NavBar() {
   return (
     <nav className="nav-bar">
       <ul className="nav-list">
-        <Link className="nav-link-menu" id="nav-link-menu-active">
-          Home
-        </Link>
+        <Link className="nav-link-menu">Home</Link>
         <Link className="nav-link-menu">Shop</Link>
         <Link className="nav-link-menu">About</Link>
         <Link className="nav-link-menu">Contact</Link>
